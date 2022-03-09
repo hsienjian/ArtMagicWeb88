f77829d734527b1bb7c043a86512775908eafeb4
Puah Hsien Jian <hsienjian@live.com>
2022-03-09 23:39:50 +0800

    added logo

--- ArtMagicWeb88/Site.Master.designer.cs
+++ ArtMagicWeb88/Site.Master.designer.cs
@@ -15,6 +15,15 @@ namespace ArtMagicWeb88
     {
 
         /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
